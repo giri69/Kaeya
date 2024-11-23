21c8cf0d5298ca887a4d5b307329de2d467550a2
giridhar <giridharpp07@gmail.com>
2024-11-24 03:11:52 +0530

    designed alerts page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,20 +10,25 @@ import Dashboard from './Page/Dashboard';
 import Connect from './Page/Connect';
 import CardsPage from './Page/Ownc';
 import DetailsPage from './Page/owncc';
+import Appp from './Popup';
+import AlertsPage from './Page/AlertsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+      
       <Route path="/" element={<App />}>
         <Route index element={<Homepage />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="card" element={<CardsPage />} />
         <Route path="card1" element={<DetailsPage />} />
+        <Route path="alerts" element={<AlertsPage />} />
       </Route>
       <Route path='/dashboard' element={<App/>}>
       <Route index element={<Dashboard />} /> 
       <Route path='connect' element={<Connect/>}/>
+      
       </Route>
       <Route path="*" element={<App />} /> 
     </>
